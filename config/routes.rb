d81467cf203593113b2eabcdb8103d824433c177
soamicharan <charansoami@gmail.com>
2019-07-02 18:09:32 +0530

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :candidate_details
+  root 'candidate_details#dashboard'
   get 'Filter', to: "filter_page#filter_menu", as:"filter"
   post "Filter_Result",to: "candidate_details#filter_result", as: "filter_result"
-  root 'candidate_details#dashboard'
-  resources :candidate_details
-  devise_for :users
   get "download_candidate_details", to:"candidate_details#downloadxlsx", as:"download"
   get "Admin_Panel", to: "admin_control#admin_panel", as:"panel"
   get "Admin_Panel_Create", to:"admin_control#admin_panel_create", as:"panel_create"
