4954b5167e5feb8efc96c62d4c152576602d25dd
soamicharan <charansoami@gmail.com>
2019-07-02 12:40:52 +0530

    updated

--- app/controllers/candidate_details_controller.rb
+++ app/controllers/candidate_details_controller.rb
@@ -12,6 +12,39 @@ class CandidateDetailsController < ApplicationController
     
     redirect_to candidate_details_path(:query => "SELECT * FROM candidate_details WHERE")
   end
+  def filter_result
+   param_list=[:src_reg,:zone,:name,:branch,:state,:status]
+    param_string=["src_reg","zone","name","branch","state","status"]
+    param_query_list=[]
+    param_query=""
+    for i in (0..5)
+      unless params[param_list[i]].blank?
+        param_query_list.push(params[param_list[i]])
+        param_query+=param_string[i]+"= ? AND "
+      end
+    end
+    unless params[:DOR].blank?
+	st_date,end_date=params[:DOR].split(' - ')
+        param_query+='("DOR" BETWEEN ? AND ?) AND '
+	param_query_list.push(st_date,end_date)
+       
+    end
+    unless params[:DOC].blank?
+	st_date,end_date=params[:DOC].split(' - ')
+        param_query+='("DOC" BETWEEN ? AND ?) AND '
+	param_query_list.push(st_date,end_date)
+    end
+    
+    complete_sql_query="SELECT * FROM candidate_details WHERE "+param_query[0..param_query.length-5]+" ;"
+    print complete_sql_query
+    if param_query==""
+      redirect_to candidate_details_path(:query => "SELECT * FROM candidate_details"),notice: "Please fill at least one field."
+    else
+      print complete_sql_query
+      redirect_to candidate_details_path(:query => [complete_sql_query,param_query_list])
+    end
+
+  end
   def show
   end
   def new
